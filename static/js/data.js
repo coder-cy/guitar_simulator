ff8786fb6e775312c69582bfa8609173869b79df
coder-cy <947518456@qq.com>
2020-04-09 23:35:57 +0800

    update

--- static/js/data.js
+++ static/js/data.js
@@ -1,8 +1,85 @@
 var musicalScaleData = {
-    "C major": [
+    "C major": [{
+            stringIndex: 4,
+            fretIndex: 3,
+            content: "1",
+        },
+        {
+            stringIndex: 3,
+            fretIndex: 0,
+            content: "2",
+        },
+        {
+            stringIndex: 3,
+            fretIndex: 2,
+            content: "3",
+        },
+        {
+            stringIndex: 3,
+            fretIndex: 3,
+            content: "4",
+        },
+        {
+            stringIndex: 2,
+            fretIndex: 0,
+            content: "5",
+        },
+        {
+            stringIndex: 2,
+            fretIndex: 2,
+            content: "6",
+        },
+        {
+            stringIndex: 1,
+            fretIndex: 0,
+            content: "7",
+        },
+        {
+            stringIndex: 1,
+            fretIndex: 1,
+            content: "i",
+        },
+    ],
+    "D": [
         {
             stringIndex: 4,
-            
-        }
+            fretIndex: 5,
+            content: "1",
+        },
+        {
+            stringIndex: 4,
+            fretIndex: 7,
+            content: "2",
+        },
+        {
+            stringIndex: 3,
+            fretIndex: 4,
+            content: "3",
+        },
+        {
+            stringIndex: 3,
+            fretIndex: 5,
+            content: "4",
+        },
+        {
+            stringIndex: 3,
+            fretIndex: 7,
+            content: "5",
+        },
+        {
+            stringIndex: 2,
+            fretIndex: 4,
+            content: "6",
+        },
+        {
+            stringIndex: 2,
+            fretIndex: 6,
+            content: "7",
+        },
+        {
+            stringIndex: 2,
+            fretIndex: 8,
+            content: "i",
+        },
     ]
 };
--- static/js/index.js
+++ static/js/index.js
@@ -4,34 +4,35 @@ const el = str => document.querySelector(str);
 class GuitarSimulator {
     constructor() {
         this.GUITAR_STRING_TOTAL = 6;
-        this.GUITAR_FRET_TOTAL = 12;
+        this.GUITAR_FRET_TOTAL = 13;
         this.fretData = {};
         this.container = el("#guitar_container");
         this.selectBox = el("#select_box");
         this.init();
     }
     init() {
-        this.renderString();
+        this.renderStrings();
+        this.renderSelectOptions();
+        this.handleMusicalScaleChange();
     }
-    renderString() {
+    renderStrings() {
         let inner = '';
         for (let i = 0; i < this.GUITAR_STRING_TOTAL; i++) {
             inner += `<ul class="string${i + 1} string">`;
-            const curFretList = [];
-            this.fretData[`string${i + 1}`] = curFretList;
+            this.fretData[`string${i + 1}`] = [];
             for (let j = 0; j < this.GUITAR_FRET_TOTAL; j++) {
                 inner += `
-                    <li class="fret${j + 1} fret">
-                        <div style="display: block" class="touch_point"></div>
-                        <div style="display: block" class="content"></div>
-                        <input type="text" style="display: none">
+                    <li class="fret${j} fret">
+                        <div style="display: none" class="touch_point"></div>
+                        <div style="display: none" class="content"></div>
+                        <input style="display: none" type="text">
                     </li>
                 `;
-                curFretList.push({
+                this.fretData[`string${i + 1}`].push({
                     stringIndex: i,
                     stringName: `${i + 1}弦`,
                     fretIndex: j,
-                    fretName: `${j + 1}品`,
+                    fretName: `${j}品`,
                     isTouched: false,
                     content: "",
                 });
@@ -39,6 +40,66 @@ class GuitarSimulator {
             inner += '</ul>';
         }
         this.container.innerHTML = inner;
+    }
+    renderSelectOptions() {
+        let inner = "";
+        for (const majorOrMinorName in musicalScaleData) {
+            const musicalScaleList = musicalScaleData[majorOrMinorName];
+            inner += `
+                <option value=${JSON.stringify(musicalScaleList)}>${majorOrMinorName}</option>
+            `;
+        }
+        this.selectBox.innerHTML = inner;
+
+    }
+    handleMusicalScaleChange() {
+        const that = this;
+        this.selectBox.onchange = function (ev) {
+            that.initFretData();
+            var ev = ev || event;
+            const tarList = JSON.parse(ev.target.value);
+            tarList.forEach(tar => {
+                const {
+                    stringIndex,
+                    fretIndex,
+                    content
+                } = tar;
+                for (const stringName in that.fretData) {
+                    if (stringName.includes(stringIndex + 1)) {
+                        that.fretData[stringName][fretIndex].isTouched = true;
+                        that.fretData[stringName][fretIndex].content = content;
+                    }
+                }
+            })
+            that.updateStrings();
+        }
+    }
+    updateStrings() {
+        for (const stringName in this.fretData) {
+            const curFretList = this.fretData[stringName];
+            const fretLis = Array.from(el("." + stringName).children);
+            fretLis.forEach((fretLi, fretLiIndex) => {
+                const touchPoint = fretLi.children[0];
+                const touchContent = fretLi.children[1];
+                const touchInput = fretLi.children[2];
+
+                touchPoint.style.display = curFretList[fretLiIndex].isTouched ? "block" : "none";
+                touchContent.style.display = curFretList[fretLiIndex].isTouched ? "block" : "none";
+                touchContent.innerHTML = curFretList[fretLiIndex].content;
+            });
+        }
+    }
+    initFretData() {
+        for (const stringName in this.fretData) {
+            this.fretData[stringName] = this.fretData[stringName].map(curFret => {
+                return {
+                    ...curFret,
+                    isTouched: false,
+                    content: "",
+                }
+            });
+        }
+        // console.log("init", this.fretData)
     }
 
 }
